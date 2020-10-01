8c7f256648f7500db6cb9051a04f930c4e1dc6a9
nenekelvin <karvinmitchar@gmail.com>
2020-10-01 12:40:44 +0300

    edit landing page

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,3 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
